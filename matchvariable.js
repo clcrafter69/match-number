cbb043f6bdd1a163858fa670a53f06245d3f1ee3
clcrafter69 <clcrafter@gmail.com>
2017-07-18 19:38:55 -0400

    adding match variables

---
+++ matchvariable.js
@@ -0,0 +1,14 @@
+//Match if divisible by 3
+
+var number =2;
+
+for (var i =1;i<=100;i++)
+{
+    if( i%number==0){
+        console.log("Match!");
+    }
+    else{
+        console.log(i);
+    }
+        
+}
